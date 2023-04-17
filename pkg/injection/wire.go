4ab4e14903f65be549d0e2790ee756de6cf8e9d4
RyotaroSeto <hahahahawaiwai321@gmail.com>
2023-04-17 21:10:03 +0900

    wire修正

--- pkg/injection/wire.go
+++ pkg/injection/wire.go
@@ -1,39 +1,42 @@
+//go:build wireinject
+// +build wireinject
+
 package injection
 
-// func InitializeAuthController() *userinterface.AuthController {
-// 	wire.Build(
-// 		infrastructure.GetDB,
-// 		wire.Bind(
-// 			new(application.Transaction),
-// 			new(*infrastructure.DB),
-// 		),
-// 		infrastructure.NewAuthRepository,
-// 		wire.Bind(
-// 			new(domain.AuthRepository),
-// 			new(*infrastructure.AuthRepository),
-// 		),
-// 		application.NewAuthService,
-// 		userinterface.NewAuthController,
-// 	)
+func InitializeAuthController() *userinterface.AuthController {
+	wire.Build(
+		infrastructure.GetDB,
+		wire.Bind(
+			new(application.Transaction),
+			new(*infrastructure.DB),
+		),
+		infrastructure.NewAuthRepository,
+		wire.Bind(
+			new(domain.AuthRepository),
+			new(*infrastructure.AuthRepository),
+		),
+		application.NewAuthService,
+		userinterface.NewAuthController,
+	)
 
-// 	return nil
-// }
+	return nil
+}
 
-// func InitializeUserController() *userinterface.UserController {
-// 	wire.Build(
-// 		infrastructure.GetDB,
-// 		wire.Bind(
-// 			new(application.Transaction),
-// 			new(*infrastructure.DB),
-// 		),
-// 		infrastructure.NewUserRepository,
-// 		wire.Bind(
-// 			new(domain.UserRepository),
-// 			new(*infrastructure.UserRepository),
-// 		),
-// 		application.NewUserService,
-// 		userinterface.NewUserController,
-// 	)
+func InitializeUserController() *userinterface.UserController {
+	wire.Build(
+		infrastructure.GetDB,
+		wire.Bind(
+			new(application.Transaction),
+			new(*infrastructure.DB),
+		),
+		infrastructure.NewUserRepository,
+		wire.Bind(
+			new(domain.UserRepository),
+			new(*infrastructure.UserRepository),
+		),
+		application.NewUserService,
+		userinterface.NewUserController,
+	)
 
-// 	return nil
-// }
+	return nil
+}
