2a7a9ac4cfcf224554e6a9dc806f735c8b1ba009
RyotaroSeto <hahahahawaiwai321@gmail.com>
2023-04-24 23:09:47 +0900

    ライクAPIの実装

--- pkg/infrastructure/like.go
+++ pkg/infrastructure/like.go
@@ -2,6 +2,8 @@ package infrastructure
 
 import (
 	"context"
+	"errors"
+	"log"
 	"now-go-kon/pkg/domain"
 	"time"
 
@@ -12,8 +14,14 @@ type Like struct {
 	UserID      int       `gorm:"column:user_id"`
 	LikedUserID int       `gorm:"column:liked_user_id"`
 	LikedDate   time.Time `gorm:"column:liked_date;autoCreateTime"`
-	Status      int       `gorm:"column:status"`
+	Status      int       `gorm:"default:1"`
 	MessageBody string    `gorm:"column:message_body"`
+}
+
+func (u *Like) fromEntity(e *domain.Like) {
+	u.UserID = e.UserID.Num()
+	u.LikedUserID = e.LikedUserID.Num()
+	u.MessageBody = e.MessageBody.String()
 }
 
 type LikeRepository struct {
@@ -36,5 +44,13 @@ func (u *LikeRepository) conn(ctx context.Context) *gorm.DB {
 }
 
 func (u *LikeRepository) CreateLike(ctx context.Context, uParam *domain.Like) error {
+	var l Like
+	l.fromEntity(uParam)
+
+	if err := u.conn(ctx).Create(&l).Error; err != nil {
+		log.Println(err)
+		return errors.New(err.Error())
+	}
+
 	return nil
 }
